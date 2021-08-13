3475d09fbfa286e679af30dbe62fd86be1b2eb73
zhouguodong <18513080058@163.com>
2021-08-13 09:45:19 +0800

    update bootstrap class

--- priactice_0004_nio/netty/src/main/java/com/example/netty/server/NettyServerApplication.java
+++ priactice_0004_nio/netty/src/main/java/com/example/netty/server/NettyServerApplication.java
@@ -3,11 +3,17 @@ package com.example.netty.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class NettyServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NettyServerApplication.class, args);
+        //SpringApplication.run(NettyServerApplication.class, args);
+        HttpServer server =new HttpServer(false,8089);
+        try {
+            server.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
