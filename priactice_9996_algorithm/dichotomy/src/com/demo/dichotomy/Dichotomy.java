0e2db4a86b5db1809829c49e5421d7b7385a3d50
FocusZhouGD <18513080058@163.com>
2021-11-18 16:54:58 +0800

    添加算法栏目

---
+++ priactice_9996_algorithm/dichotomy/src/com/demo/dichotomy/Dichotomy.java
@@ -0,0 +1,36 @@
+package com.demo.dichotomy;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Dichotomy {
+
+    public static void main(String[] args) {
+
+        List<Integer> i = Arrays.asList(0, 1, 2, 3, 5, 6);
+        int start = 1;
+        int end = i.size();
+        int middle = (start + end) / 2;
+
+        int result = -1;
+        while (end > start) {
+            if (i.get(start) != start) {
+                result = start - 1;
+                break;
+            }
+            if (i.get(end - 1) == end - 1) {
+                result = end - 1;
+                break;
+            }
+            if (i.get(middle) != middle) {
+                end = middle;
+            } else {
+                start = middle;
+            }
+            middle = (start + end) / 2;
+
+        }
+        System.out.println(i.get(result) + 1);
+    }
+
+}
