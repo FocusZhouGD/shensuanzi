925496414c6e1568e73cc92180d40257e4286538
FocusZhouGD <18513080058@163.com>
2021-12-24 17:58:18 +0800

    添加配置文件 start

---
+++ priactice_0005_spring/custom-starter-test/src/main/java/com/example/custom/starter/test/CustomStarterTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.custom.starter.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomStarterTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomStarterTestApplication.class, args);
+    }
+
+}
