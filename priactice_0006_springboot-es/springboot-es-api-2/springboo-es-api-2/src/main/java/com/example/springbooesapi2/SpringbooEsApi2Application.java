88a92ad9c317715db69d2c5ab9ce74668b6b3e4c
zhouguodong <18513080058@163.com>
2022-05-11 14:52:46 +0800

    初始化es2依赖

---
+++ priactice_0006_springboot-es/springboot-es-api-2/springboo-es-api-2/src/main/java/com/example/springbooesapi2/SpringbooEsApi2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbooesapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbooEsApi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbooEsApi2Application.class, args);
+    }
+
+}
