4d7bd5597ac188afe7ef7cacf9b62292e2693d06
zhouguodong <18513080058@163.com>
2022-05-12 22:18:46 +0800

    初始化es2依赖002

---
+++ priactice_0006_springboot-es/springboot-es-api-3/springboot-es-api-3/src/test/java/com/example/springbootesapi3/SpringbootEsApi3ApplicationTests.java
@@ -0,0 +1,30 @@
+package com.example.springbootesapi3;
+
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
+class SpringbootEsApi3ApplicationTests {
+
+    @Autowired
+    private RestHighLevelClient restHighLevelClient;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testCreateIndex() throws IOException {
+        CreateIndexRequest createIndexRequest =new CreateIndexRequest("my_zgd");
+        CreateIndexResponse createIndexResponse = restHighLevelClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+        System.out.println(createIndexResponse);
+    }
+
+}
